1d5cf720a42d50aaad0e5dc8bb5a87065f31f938
Subham Negi <negisubham210@gmail.com>
2026-09-03 22:24:11 +0530

    commit

---
+++ src/first.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+import ReactDOM from "react-dom/client";
+
+
+function App(){
+    return(
+        <h1>Hello World</h1>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App></App>);
